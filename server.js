2962d460e98672653e93c16d5e38eb5101c8f5c8
Francisco Machado <franm912@gmail.com>
2016-10-06 02:47:15 +0100

    First working version

---
+++ server.js
@@ -0,0 +1,21 @@
+// Set listening port
+var port = process.envPORT || 3000;
+
+// Import express module and create app object
+var express = require('express');
+var app = express();
+
+// Set route
+var router = express.Router();
+router.get('/', function (req, resp) {
+    var software = req.headers['user-agent'];
+    software = software.slice(software.indexOf('(') + 1, software.indexOf(')'))
+    resp.json({
+        ipaddress: req.connection.remoteAddress,
+        language: req.acceptsLanguages()[0],
+        software: software
+    });
+});
+
+app.use('/', router)
+app.listen(port);
